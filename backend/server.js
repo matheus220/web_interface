06a97da14a24ade6e93be214536750dc1ce25b5a
Matheus Lessa <matheus.220@hotmail.com>
2019-08-07 04:28:35 +0200

    Add npm run server

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const missionRoutes = express.Router();
 const logmissionRoutes = express.Router();
 const logRoutes = express.Router();
 const scheduleRoutes = express.Router();
-const PORT = 4000;
 
 later.date.localTime();
 let filePath = "/tmp/crontabFile.txt";
@@ -345,14 +344,19 @@ scheduleRoutes.route('/delete/:id').post(function(req, res) {
 
 app.use('/api/task', scheduleRoutes);
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, './../build')));
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    console.log("Server running in PRODUCTION mode");
+    // Set static folder
+    app.use(express.static(path.join(__dirname, './../build')));
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'./../build/index.html'));
-});
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname+'./../build/index.html'));
+    });
+}
 
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
+const port = process.env.PORT || 4000;
+
+app.listen(port, function() {
+    console.log("Server is running on Port: " + port);
 });
